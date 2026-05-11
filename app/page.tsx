1b53821f9cf4ddc77b664e40db930efa0be9a6c9
Tomas <tomaslonnblad@gmail.com>
2026-05-11 18:25:07 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import TimeMachine from "@/components/time-machine"
 
 export default function Page() {
   return (
-   <main className=" object-cover bg-black">
+   <main className=" w-full h-screen object-fit bg-black">
       <TimeMachine />
     </main>
   )
