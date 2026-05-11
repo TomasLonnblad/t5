bab8945dabb770b54f842ba772bb20cd87edf40d
Tomas <tomaslonnblad@gmail.com>
2026-05-11 18:23:04 +0200

    Remove height from main element in page component

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import TimeMachine from "@/components/time-machine"
 
 export default function Page() {
   return (
-   <main className="w-full h-screen object-cover bg-black">
+   <main className=" object-cover bg-black">
       <TimeMachine />
     </main>
   )
